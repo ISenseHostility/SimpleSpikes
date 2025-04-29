3c7c94ba1a427bd96ed26e7dcb4c597f89b1f995
ISenseHostility <yaboijarno@gmail.com>
2025-04-29 19:39:29 +0200

    Refactor registration

--- common/src/main/java/tech/jarno/simple_spikes/block/ModBlocks.java
+++ common/src/main/java/tech/jarno/simple_spikes/block/ModBlocks.java
@@ -3,8 +3,11 @@ package tech.jarno.simple_spikes.block;
 import net.blay09.mods.balm.api.block.BalmBlocks;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static tech.jarno.simple_spikes.SimpleSpikes.id;
 
@@ -16,6 +19,7 @@ public class ModBlocks {
     public static Block netheriteSpikes;
     public static Block slimeSpikes;
     public static Block stoneSpikes;
+    public static Block cobblestoneSpikes;
     public static Block oakSpikes;
     public static Block darkOakSpikes;
     public static Block birchSpikes;
@@ -27,31 +31,50 @@ public class ModBlocks {
     public static Block mangroveSpikes;
     public static Block cherrySpikes;
     public static Block paleOakSpikes;
-    public static Block cobblestoneSpikes;
+
+    private record SpikeEntry(
+            String name,
+            Function<ResourceKey<Block>, Block> factory,
+            Consumer<Block> setter
+    ) {}
+
+    private static final List<SpikeEntry> SPIKES = List.of(
+            new SpikeEntry("copper_spikes", CopperSpikes::new, b -> copperSpikes = b),
+            new SpikeEntry("diamond_spikes", DiamondSpikes::new, b -> diamondSpikes = b),
+            new SpikeEntry("gold_spikes", GoldSpikes::new, b -> goldSpikes = b),
+            new SpikeEntry("iron_spikes", IronSpikes::new, b -> ironSpikes = b),
+            new SpikeEntry("netherite_spikes", NetheriteSpikes::new, b -> netheriteSpikes = b),
+            new SpikeEntry("slime_spikes", SlimeSpikes::new, b -> slimeSpikes = b),
+
+            // stone variants
+            new SpikeEntry("stone_spikes", StoneSpikes::new, b -> stoneSpikes = b),
+            new SpikeEntry("cobblestone_spikes", StoneSpikes::new, b -> cobblestoneSpikes = b),
+
+            // wooden variants
+            new SpikeEntry("oak_spikes", WoodenSpikes::new, b -> oakSpikes = b),
+            new SpikeEntry("dark_oak_spikes", WoodenSpikes::new, b -> darkOakSpikes = b),
+            new SpikeEntry("birch_spikes", WoodenSpikes::new, b -> birchSpikes = b),
+            new SpikeEntry("jungle_spikes", WoodenSpikes::new, b -> jungleSpikes = b),
+            new SpikeEntry("acacia_spikes", WoodenSpikes::new, b -> acaciaSpikes = b),
+            new SpikeEntry("spruce_spikes", WoodenSpikes::new, b -> spruceSpikes = b),
+            new SpikeEntry("warped_spikes", WoodenSpikes::new, b -> warpedSpikes = b),
+            new SpikeEntry("crimson_spikes", WoodenSpikes::new, b -> crimsonSpikes = b),
+            new SpikeEntry("cherry_spikes", WoodenSpikes::new, b -> cherrySpikes = b),
+            new SpikeEntry("mangrove_spikes", WoodenSpikes::new, b -> mangroveSpikes = b),
+            new SpikeEntry("pale_oak_spikes", WoodenSpikes::new, b -> paleOakSpikes = b)
+    );
 
     public static void initialize(BalmBlocks blocks) {
-        blocks.registerBlock((location) -> ModBlocks.copperSpikes = new CopperSpikes(createBlockKey(location)), id("copper_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.diamondSpikes = new DiamondSpikes(createBlockKey(location)), id("diamond_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.goldSpikes = new GoldSpikes(createBlockKey(location)), id("gold_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.ironSpikes = new IronSpikes(createBlockKey(location)), id("iron_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.netheriteSpikes = new NetheriteSpikes(createBlockKey(location)), id("netherite_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.slimeSpikes = new SlimeSpikes(createBlockKey(location)), id("slime_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.stoneSpikes = new StoneSpikes(createBlockKey(location)), id("stone_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.cobblestoneSpikes = new StoneSpikes(createBlockKey(location)), id("cobblestone_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.oakSpikes = new WoodenSpikes(createBlockKey(location)), id("oak_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.darkOakSpikes = new WoodenSpikes(createBlockKey(location)), id("dark_oak_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.jungleSpikes = new WoodenSpikes(createBlockKey(location)), id("jungle_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.birchSpikes = new WoodenSpikes(createBlockKey(location)), id("birch_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.acaciaSpikes = new WoodenSpikes(createBlockKey(location)), id("acacia_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.spruceSpikes = new WoodenSpikes(createBlockKey(location)), id("spruce_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.warpedSpikes = new WoodenSpikes(createBlockKey(location)), id("warped_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.crimsonSpikes = new WoodenSpikes(createBlockKey(location)), id("crimson_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.cherrySpikes = new WoodenSpikes(createBlockKey(location)), id("cherry_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.mangroveSpikes = new WoodenSpikes(createBlockKey(location)), id("mangrove_spikes"));
-        blocks.registerBlock((location) -> ModBlocks.paleOakSpikes = new WoodenSpikes(createBlockKey(location)), id("pale_oak_spikes"));
-    }
-
-    private static ResourceKey<Block> createBlockKey(ResourceLocation location) {
-        return ResourceKey.create(Registries.BLOCK, location);
+        for (SpikeEntry entry : SPIKES) {
+            blocks.registerBlock(
+                    location -> {
+                        ResourceKey<Block> key = ResourceKey.create(Registries.BLOCK, location);
+                        Block b = entry.factory.apply(key);
+                        entry.setter.accept(b);
+                        return b;
+                    },
+                    id(entry.name)
+            );
+        }
     }
 }
